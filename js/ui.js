995001fd7514c0513ba20c4c37afcd438261b7a0
MikePena <migpena012@gmail.com>
2025-12-24 09:09:47 -0500

    agregar diseño responsive en ambos html

--- js/ui.js
+++ js/ui.js
@@ -1,29 +1,29 @@
 function renderCountries(countries) {
-    const container = document.querySelector(".countries-container");
-    
-    container.innerHTML = "";
-    
-    for (const country of countries) {
-        const card = generateCard(country);
+  const container = document.querySelector(".countries-container");
 
-        card.addEventListener("click", () => {
-            window.location.href = `detail.html?code=${country.cca3}`;
-        });
-        
-        container.append(card);
-    };
+  container.innerHTML = "";
+
+  for (const country of countries) {
+    const card = generateCard(country);
+
+    card.addEventListener("click", () => {
+      window.location.href = `detail.html?code=${country.cca3}`;
+    });
+
+    container.append(card);
+  }
 }
 
 function generateCard(country) {
-    const card = document.createElement("li");
+  const card = document.createElement("li");
 
-    card.classList.add("card")
+  card.classList.add("card");
 
-    const flagSrc = country.flags?.svg || country.flags?.png || "";
-    const alt = country.flags?.alt || country.name.common;
+  const flagSrc = country.flags?.svg || country.flags?.png || "";
+  const alt = country.flags?.alt || country.name.common;
 
-    card.innerHTML = `
-        <div class="card-wrapper">
+  card.innerHTML = `
+        <div class="card__flag-wrapper">
             <img class="card__flag" src="${flagSrc}" alt="${alt}" />
         </div>
         <div class="card__info">
@@ -31,10 +31,12 @@ function generateCard(country) {
             <div class="card__details">
                 <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
                 <p><strong>Region:</strong> ${country.region}</p>
-                <p><strong>Capital:</strong> ${country.capital?.[0] || "N/A"}</p>
+                <p><strong>Capital:</strong> ${
+                  country.capital?.[0] || "N/A"
+                }</p>
             </div>
         </div>
-    `
+    `;
 
-    return card;
+  return card;
 }
