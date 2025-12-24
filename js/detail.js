8bbbe55b5d89f2fe80417b232a0f2f9d13121640
MikePena <migpena012@gmail.com>
2025-12-24 00:01:40 -0500

    agregar estilo a detail.html

--- js/detail.js
+++ js/detail.js
@@ -27,28 +27,36 @@ function renderCountryDetails(country) {
 
   const currencies = country.currencies ? Object.values(country.currencies).map(c => c.name).join(", ") : "N/A";
   const languages = country.languages ? Object.values(country.languages).join(", ") : "N/A";
+  const nativeName = Object.values(country.name.nativeName)[0].common || country.common;
 
   container.innerHTML = `
-    <div class="details details-container">
+      <div class="flag-wrapper">
         <img class="details__flag" src="${country.flags.svg}" alt="${country.flags.alt}" />
+      </div>
         
-        <div class="details-info">
-            <h2>${country.name.common}</h2>
-            <p><strong>Native Name:</strong> ${country.nativeName}</p>
+      <div class="details__text">
+        <h2 class="details__country-name">${country.name.common}</h2>
+        
+        <div class="details_country-info_container">
+          <div class="country-info_column">
+            <p><strong>Native Name:</strong> ${nativeName}</p>
             <p><strong>Population:</strong> ${country.population}</p>
             <p><strong>Region:</strong> ${country.region}</p>
             <p><strong>Sub Region:</strong> ${country.subregion || "N/A"}</p>
             <p><strong>Capital:</strong> ${country.capital?.[0] || "N/A"}</p>
-            <p><strong>Top Level Domain:</strong> ${country.tld}</p>
+          </div>
+          <div class="country-info_column">
+            <p><strong>Top Level Domain:</strong> ${country.tld[0]}</p>
             <p><strong>Currencies:</strong> ${currencies}</p>
             <p><strong>Languages:</strong> ${languages}</p>
-
-            <div class="borders">
-                <p><strong>Border Countries:</strong><p>
-                <div class="borders-container"></div>
-            </div>
+          </div>
         </div>
-    </div>
+        
+        <div class="borders">
+          <p><strong>Border Countries:</strong><p>
+          <div class="borders-container"></div>
+        </div>
+      </div>
     `;
 
   renderBorders(country.borders);
@@ -58,7 +66,9 @@ async function renderBorders(borders) {
   const container = document.querySelector(".borders-container");
 
   if (!borders) {
-    container.textContent = "None";
+    container.innerHTML = `
+      <p>None</p>
+      `;
     return;
   }
 
@@ -66,7 +76,9 @@ async function renderBorders(borders) {
     const borderCountry = await getCountryByCode(code); 
 
     const button = document.createElement("button");
+    button.classList.add("border-country__btn")
     button.textContent = borderCountry[0].name.common;
+    button.title = borderCountry[0].name.common;
 
     button.addEventListener("click", () => {
       window.location.href = `detail.html?code=${code}`;
